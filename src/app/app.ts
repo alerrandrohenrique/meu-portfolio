2003f6a9fb554346bb3ee737130a7a468efc6eb1
alerrandro henrique queiroz dos santos <alehenriquejogos@gmail.com>
2026-09-05 00:39:54 -0300

    feat: estrutura de pastas, componentes e rotas da etapa 4

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 import { Header } from './shared/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, RouterLink],
+  imports: [RouterOutlet, Header],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('meu-portfolio');
 }
